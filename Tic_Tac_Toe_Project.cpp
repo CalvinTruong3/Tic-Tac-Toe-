64100160c06c98fc2a31808c03c0ae6e75d29103
CalvinTruong3 <37942121+CalvinTruong3@users.noreply.github.com>
2018-04-03 15:47:43 -0700

    Update Tic_Tac_Toe_Project.cpp

--- Tic_Tac_Toe_Project.cpp
+++ Tic_Tac_Toe_Project.cpp
@@ -6,10 +6,12 @@ int row, column, diagonal;
 int matrix[3][3]; // create a 3x3 matrix
 int emptyrow(int c);
 int emptycol(int r);
+
 void init_matrix(void); // initialize matrix
 void get_player_move(void);
 void get_computer_move(void);
 void display_matrix(void);
+
 char check(void);
 bool vulnerable(int mat[3][3]);
 bool winnable(int mat[3][3]);
@@ -17,11 +19,12 @@ bool finish(int mat[3][3]);
 void restart();
 
 int main(void)
-{	
+{
 	char done, c;
 	bool CONTINUE = true;
 	cout << "Player vs. CPU Tic Tac Toe.\n";
-	cout << "The instructions are simple. You will be playing as 'X'. The CPU will be playing as 'O'. The user will have to enter in the numbers ranging from 1-3 followed by a space and another number. (Format Example: 1 2) Good Luck!\n " << endl;
+	cout << "Instructions: You will be playing as 'X'. The CPU will be playing as 'O'. Enter two numbers from 1-3 followed by a space";
+	cout << " in between. (Ex: 1 2) \n" << endl;
 	while (CONTINUE)
 	{
 		done = ' ';
@@ -49,28 +52,29 @@ int main(void)
 				break;
 			}
 			restart();
-		
 		} while (done == ' ');
-		
+
 		if (done == 'X')
 		{
 			cout << "\nPlayer has won the match! \n";
 		}
+
 		if (done == 'O')
 		{
 			cout << "\nCPU has won the match! \n";
 		}
+
 		display_matrix();  // shows the final matrix display
 		cout << "Play again? (Y/N)" << endl;
 		cin >> c;
-		
+
 		if (c == 'N' || c == 'n')
 		{
 			CONTINUE = false;
 		}
 		restart();
 	}
-    return 0;
+	return 0;
 }
 
 int emptyrow(int c)
@@ -84,7 +88,7 @@ int emptyrow(int c)
 	}
 }
 
-int emptycol(int r) 
+int emptycol(int r)
 {
 	for (int i = 0; i < 3; ++i)
 	{
@@ -95,13 +99,11 @@ int emptycol(int r)
 	}
 }
 
-void init_matrix(void) 
+void init_matrix(void)
 {
-	int i, j;
-	
-	for (i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 3; j++)
+		for (int j = 0; j < 3; j++)
 		{
 			matrix[i][j] = ' ';
 		}
@@ -111,11 +113,9 @@ void init_matrix(void)
 void get_player_move(void) // calls the function for user's input
 {
 	int x, y;
-
 	cout << "Enter the coordinates to place your piece (x, y): ";
 	cin >> x >> y;
-
-	x--;y--;
+	x--; y--;
 
 	if (matrix[x][y] != ' ')
 	{
@@ -130,16 +130,19 @@ void get_computer_move(void)
 	char temporary;
 	int test[3][3], temp[3][3];
 	for (int i = 0; i < 3; ++i)
+	{
 		for (int j = 0; j < 3; ++j)
 		{
 			test[i][j] = matrix[i][j];
 			temp[i][j] = matrix[i][j];
 		}
+	}
 
 	int i, j;
-	for (i=0; i < 3; i++)
+	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
+		{
 			if (matrix[i][j] == ' ')
 			{
 				temporary = matrix[i][j];
@@ -160,23 +163,20 @@ void get_computer_move(void)
 					if (diagonal == 1)
 					{
 						if (matrix[0][0] != 'X' && matrix[1][1] != 'X' && matrix[2][2] != 'X')
-						{
 							matrix[0][0] = 'O'; matrix[1][1] = 'O'; matrix[2][2] = 'O';
-						}
 						return;
 					}
 					else
 					{
 						if (matrix[0][2] != 'X' && matrix[1][1] != 'X' && matrix[2][0] != 'X')
-						{
 							matrix[0][2] = 'O'; matrix[1][1] = 'O'; matrix[2][0] = 'O';
-						}
 						return;
 					}
 					return; // break;
 				}
 				test[i][j] = temporary;
 			}
+		}
 		if (matrix[i][j] == ' ')
 		{
 			temporary = matrix[i][j];
@@ -191,25 +191,19 @@ void get_computer_move(void)
 				if (column > 0)
 				{
 					if (matrix[emptyrow(column - 1)][column - 1] != 'X')
-					{
 						matrix[emptyrow(column - 1)][column - 1] = 'O';
-					}
 					return;
 				}
 				if (diagonal == 1)
 				{
 					if (matrix[0][0] != 'X' && matrix[1][1] != 'X' && matrix[2][2] != 'X')
-					{
 						matrix[0][0] = 'O'; matrix[1][1] = 'O'; matrix[2][2] = 'O';
-					}
 					return;
 				}
 				else
 				{
 					if (matrix[0][2] != 'X' && matrix[1][1] != 'X' && matrix[2][0] != 'X')
-					{
 						matrix[0][2] = 'O'; matrix[1][1] = 'O'; matrix[2][0] = 'O';
-					}	
 					return;
 				}
 				return; // break
@@ -220,6 +214,7 @@ void get_computer_move(void)
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
+		{
 			if (matrix[i][j] == ' ')
 			{
 				temporary = matrix[i][j];
@@ -230,6 +225,7 @@ void get_computer_move(void)
 				}
 				test[i][j] = temporary;
 			}
+		}
 		if (matrix[i][j] == ' ')
 		{
 			temporary = matrix[i][j];
@@ -244,22 +240,18 @@ void get_computer_move(void)
 	if (column > 0)
 	{
 		if (matrix[emptyrow(column - 1)][column - 1] != 'X')
-		{
 			matrix[emptyrow(column - 1)][column - 1] = 'O';
-		}
 		return;
 	}
 	if (row > 0)
 	{
 		if (matrix[row - 1][emptycol(row - 1)] != 'X')
-		{
 			matrix[row - 1][emptycol(row - 1)] = 'O';
-		}
 		return;
 	}
 	if (diagonal == 1)
 	{
-		if (matrix[0][0] != 'X') 
+		if (matrix[0][0] != 'X')
 			matrix[0][0] = 'O';
 		if (matrix[1][1] != 'X')
 			matrix[1][1] = 'O';
@@ -282,13 +274,11 @@ void get_computer_move(void)
 }
 
 void display_matrix(void) // displays the matrix
-{ 
+{
 	int d;
-
 	for (d = 0; d < 3; d++)
 	{
 		cout << " " << char(matrix[d][0]) << " | " << char(matrix[d][1]) << " | " << char(matrix[d][2]);
-
 		if (d != 2)
 		{
 			cout << "\n---|---|---\n";
@@ -300,7 +290,6 @@ void display_matrix(void) // displays the matrix
 char check(void) // used to determine winner
 {
 	int i;
-
 	for (i = 0; i < 3; i++) // Checks the rows
 	{
 		if (matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2])
@@ -329,6 +318,7 @@ char check(void) // used to determine winner
 	return ' ';
 }
 
+//Looks for all of the possible outcome of the user winning the game
 bool vulnerable(int mat[3][3])
 {
 	char a, b, c, d, e, f, g, h, i;
@@ -337,47 +327,48 @@ bool vulnerable(int mat[3][3])
 	g = mat[2][0], h = mat[2][1], i = mat[2][2];
 	if ((a == 'X' && b == 'X' && c != 'O') || (b == 'X' && c == 'X' && a != 'O') || (a == 'X' && c == 'X' && b != 'O'))
 	{
-		row = 1; 
+		row = 1;
 		return true;
 	}
 	if ((d == 'X' && e == 'X' && f != 'O') || (e == 'X' && f == 'X' && d != 'O') || (d == 'X' && f == 'X' && e != 'O'))
 	{
-		row = 2; 
+		row = 2;
 		return true;
 	}
 	if ((g == 'X' && h == 'X' && i != 'O') || (h == 'X' && i == 'X' && g != 'O') || (g == 'X' && i == 'X' && h != 'O'))
 	{
-		row = 3; 
+		row = 3;
 		return true;
 	}
 	if ((a == 'X' && d == 'X' && g != 'O') || (d == 'X' && g == 'X' && a != 'O') || (a == 'X' && g == 'X' && d != 'O'))
 	{
-		column = 1; 
+		column = 1;
 		return true;
 	}
 	if ((b == 'X' && e == 'X' && h != 'O') || (e == 'X' && h == 'X' && b != 'O') || (b == 'X' && h == 'X' && e != 'O'))
 	{
-		column = 2; 
+		column = 2;
 		return true;
 	}
 	if ((c == 'X' && f == 'X' && i != 'O') || (f == 'X' && i == 'X' && c != 'O') || (c == 'X' && i == 'X' && f != 'O'))
 	{
-		column = 3; 
+		column = 3;
 		return true;
 	}
 	if ((a == 'X' && e == 'X' && i != 'O') || (e == 'X' && i == 'X' && a != 'O') || (a == 'X' && i == 'X' && e != 'O'))
 	{
-		diagonal = 1; 
+		diagonal = 1;
 		return true;
 	}
 	if ((g == 'X' && e == 'X' && c != 'O') || (e == 'X' && c == 'X' && g != 'O') || (g == 'X' && c == 'X' && e != 'O'))
 	{
-		diagonal = 2; 
+		diagonal = 2;
 		return true;
 	}
 	return false;
 }
 
+//Looks for all of the possible outcome of the computer winning the game
 bool winnable(int mat[3][3])
 {
 	char a, b, c, d, e, f, g, h, i;
@@ -427,6 +418,7 @@ bool winnable(int mat[3][3])
 	return false;
 }
 
+//Looks for the to see if the spaces are all filled out before ending the game
 bool finish(int mat[3][3])
 {
 	for (int i = 0; i < 3; ++i)
@@ -448,3 +440,4 @@ void restart()
 	column = 0;
 	diagonal = 0;
 }
+
